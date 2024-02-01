c405253838994c353731b0325f217027e80eb6cd
nik-agarkar <88163149+nik-agarkar@users.noreply.github.com>
2024-02-01 09:12:06 -0500

    Create file

---
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.java
@@ -0,0 +1,19 @@
+            
+           swap(arr,i,j);
+            
+                j--;
+            while(i<j && !isVowel(arr[j]))
+            // while(i<j && !vowels.contains(arr[j]+""))
+                i++;
+            while(i<j && !isVowel(arr[i]))
+            // while(i<j && !vowels.contains(arr[i]+""))
+        while(i<j){
+        
+        char[] arr = s.toCharArray();
+        int i = 0, j = s.length()-1;
+        // String vowels = "aeiouAEIOU";
+        if(s==null || s.length()==0) return s;
+    public String reverseVowels(String s) {
+    
+class Solution {
+"
