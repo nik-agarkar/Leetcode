0feb8864a5684bfd13329de4325de6f07705b0f9
nik-agarkar <88163149+nik-agarkar@users.noreply.github.com>
2024-02-11 14:07:01 -0500

    Create file

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -0,0 +1,19 @@
+
+            if(arr[i]==1){
+                i+=2;
+            }
+            else if(arr[i]==0){
+                if(i==arr.length-1 || (i+1<arr.length && arr[i+1]==0)){
+                    n-=1;
+                    if(n==0) 
+                        return true;
+                    else
+                        i+=2;
+                }
+                else
+        for(int i=0;i<arr.length;){  
+
+        if(n==0) return true;
+    public boolean canPlaceFlowers(int[] arr, int n) {
+class Solution {
+[1,0,0,0,1]
