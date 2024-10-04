2536cd3366aaaf64af52c95263f6732650c0ff81
nik-agarkar <88163149+nik-agarkar@users.noreply.github.com>
2024-10-04 16:40:52 -0400

    Time: 50 ms (16.48%) | Memory: 67.4 MB (5.32%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,28 +1,19 @@
 class Solution {
+
     public int[] arrayRankTransform(int[] arr) {
-        TreeMap<Integer, Integer> tmap =new TreeMap<>();
-
-        for(int val:arr) {
-            tmap.put(val, tmap.getOrDefault(val,0)+1);
+        // Store the rank for each number in arr
+        HashMap<Integer, Integer> numToRank = new HashMap<>();
+        // Deduplicate and sort arr
+        TreeSet<Integer> nums = new TreeSet<>();
+        for (int num : arr) nums.add(num);
+        int rank = 1;
+        for (int num : nums) {
+            numToRank.put(num, rank);
+            rank++;
         }
-
-        int rank =1;
-        for(Map.Entry<Integer,Integer> entry: tmap.entrySet()) {
-            tmap.put(entry.getKey(), rank);
-            rank+=1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = numToRank.get(arr[i]);
         }
-
-        int[] res=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            res[i] = tmap.get(arr[i]);
-        }
-
-        return res;
+        return arr;
     }
 }
-
-/**
-Input: arr = [40,10,20,30]
-Output: [4,1,2,3]
-
- */
